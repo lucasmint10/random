ef6e82555b48899039c2bd7c1c38b83dcf150538
Lucas Mint <117103405+lucasmint10@users.noreply.github.com>
2026-05-20 16:38:22 -0300

    Update catalog.js

--- src/catalog.js
+++ src/catalog.js
@@ -1,10 +1,7 @@
 export const videoCatalog = [
     {
-        id: "yt-qaqnljWV42M",
-        type: "youtube",
-        youtubeId: "qaqnljWV42M",
+        id: "qaqnljWV42M",
         title: "turkish march but its not march",
         description: "None.",
-        publishedAt: "2023-6-26T00:00:00Z"
     }
 ];
