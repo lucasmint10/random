f3c1897c5051a9648e6ff6e30d586358d5ca1e5b
Lucas Mint <117103405+lucasmint10@users.noreply.github.com>
2026-05-20 16:44:39 -0300

    added videos

--- src/catalog.js
+++ src/catalog.js
@@ -2,6 +2,21 @@ export const videoCatalog = [
     {
         id: "qaqnljWV42M",
         title: "turkish march but its not march",
-        description: "None.",
+        description: "why.",
+    },
+    {
+        id: "tW3oXdl1E64",
+        title: "BAD APPLE!! - Piano Tutorial",
+        description: "BAD APPLE! COOL.",
+    },
+    {
+        id: "1Zrq8FiKS6A",
+        title: "At the Speed of Light",
+        description: "Bloodbath mention?",
+    },
+    {
+        id: "Iq7j441JMbA",
+        title: "Flamewall",
+        description: "GD MENTION!",
     }
 ];
