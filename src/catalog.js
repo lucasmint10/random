bfc5c21332fccd0e5da609c58292370ecfa94145
Lucas Mint <117103405+lucasmint10@users.noreply.github.com>
2026-05-20 16:52:46 -0300

    Update catalog.js

--- src/catalog.js
+++ src/catalog.js
@@ -18,5 +18,25 @@ export const videoCatalog = [
         id: "Iq7j441JMbA",
         title: "Flamewall",
         description: "GD MENTION!",
+    },
+    {
+        id: "RLNN5SIJeUc",
+        title: "Operation: Evolution",
+        description: "COOL MUSIC.",
+    },
+    {
+        id: "m7wtUsNxJI0",
+        title: "can can but i can't",
+        description: "piano explosion!",
+    },
+    {
+        id: "Flk_A16_EJ4",
+        title: "CRIM3S - lost (XVA Remix)",
+        description: "Creppy...",
+    },
+    {
+        id: "0hyklaV6st8",
+        title: "William Tell Overture but the pianist has ADHD.",
+        description: "Pianist really has ADHD.",
     }
 ];
